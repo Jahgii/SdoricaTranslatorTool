1cd09b03c377c8466567e2fb93b351406880e742
Jahg <jahg.huerta@gmail.com>
2023-11-14 18:43:44 -0800

    boostrap app using standalone APIs

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -2,20 +2,43 @@ import { Component } from '@angular/core';
 import { LanguageOriginService } from './core/services/language-origin.service';
 import { AuthService } from './core/services/auth.service';
 import { ThemeService } from './core/services/theme.service';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { TuiMarkerIconModule } from '@taiga-ui/kit';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { ThemeDarkComponent } from './components/theme-dark/theme-dark.component';
+import { NgIf, NgTemplateOutlet, AsyncPipe } from '@angular/common';
+import { TuiRootModule, TuiThemeNightModule } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [
+    NgTemplateOutlet,
+    RouterOutlet,
+    RouterLink,
+    NgIf,
+    AsyncPipe,
+
+    //Taiga UI
+    TuiRootModule,
+    TuiMarkerIconModule,
+    TuiThemeNightModule,
+
+    //App Components
+    ThemeDarkComponent,
+    NavMenuComponent,
+    NavBarComponent,
+  ]
 })
 export class AppComponent {
 
-  title = 'app';
+  title = 'Translator Tool';
 
   constructor(
     public languageOrigin: LanguageOriginService,
     public authService: AuthService,
     public theme: ThemeService
-  ) {
-    // this.languageOrigin.onRetriveLanguages();
-  }
+  ) { }
 }
--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -1,20 +1,106 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { HttpLoaderFactory } from './app/app.module';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { JwtModule } from '@auth0/angular-jwt';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TuiSidebarModule, TuiAppBarModule } from '@taiga-ui/addon-mobile';
+import { TuiAutoFocusModule, TuiActiveZoneModule, TuiLetModule } from '@taiga-ui/cdk';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { TuiTableModule, TuiTableFiltersModule } from '@taiga-ui/addon-table';
+import { TuiBlockStatusModule } from '@taiga-ui/layout';
+import { TuiInputFilesModule, TuiStepperModule, TuiIslandModule, TuiMarkerIconModule, TuiProgressModule, TuiCheckboxBlockModule, TuiCheckboxModule, TuiInputModule, TuiFilterModule, TuiTilesModule, TuiSelectModule, TuiDataListWrapperModule, TuiTabsModule, TuiTextareaModule, TuiToggleModule, TuiInputInlineModule, TuiBadgeModule, TuiInputNumberModule, TuiComboBoxModule, TuiFilterByInputPipeModule, TuiRadioBlockModule, TuiAvatarModule } from '@taiga-ui/kit';
+import { withInterceptorsFromDi, provideHttpClient, HttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { TuiLanguageName } from '@taiga-ui/i18n/interfaces';
+import { tuiLanguageSwitcher } from '@taiga-ui/i18n/switch';
+import { of } from 'rxjs';
+import { TUI_LANGUAGE, TUI_ENGLISH_LANGUAGE } from '@taiga-ui/i18n';
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import { TUI_SANITIZER, TuiRootModule, TuiDialogModule, TuiAlertModule, TuiThemeNightModule, TuiModeModule, TuiButtonModule, TuiSvgModule, TuiLoaderModule, TuiScrollbarModule, TuiTextfieldControllerModule, TuiDataListModule, TuiTooltipModule, TuiHintModule, TuiGroupModule, TuiDropdownModule, TuiHostedDropdownModule, TuiExpandModule, TuiLinkModule } from '@taiga-ui/core';
+import { environment as environment_1 } from 'src/environments/environment';
+import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule, GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
 
 export function getBaseUrl() {
-  return document.getElementsByTagName('base')[0].href;
+    return document.getElementsByTagName('base')[0].href;
 }
-
-const providers = [
-  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }
-];
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
-platformBrowserDynamic(providers).bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(
+            BrowserModule,
+            AppRoutingModule,
+            FormsModule,
+            ReactiveFormsModule,
+
+            //Login
+            SocialLoginModule,
+            GoogleSigninButtonModule,
+            
+            //Taiga
+            TuiRootModule,
+            TuiThemeNightModule,
+            
+            TranslateModule.forRoot({
+                defaultLanguage: 'es',
+                loader: {
+                    provide: TranslateLoader,
+                    useFactory: HttpLoaderFactory,
+                    deps: [HttpClient]
+                }
+            }),
+            JwtModule.forRoot({
+                config: {
+                    tokenGetter: () => localStorage.getItem("token"),
+                    allowedDomains: [environment.allowedDomains]
+                },
+            })),
+        {
+            provide: 'BASE_URL', useFactory: getBaseUrl, deps: []
+        },
+        {
+            provide: 'SocialAuthServiceConfig',
+            useValue: {
+                autoLogin: false,
+                providers: [
+                    {
+                        id: GoogleLoginProvider.PROVIDER_ID,
+                        provider: new GoogleLoginProvider(environment.googleClientId)
+                    }
+                ],
+                onError: (err) => {
+                    console.error(err);
+                }
+            } as SocialAuthServiceConfig,
+        },
+        {
+            provide: TUI_SANITIZER,
+            useClass: NgDompurifySanitizer
+        },
+        {
+            provide: TUI_LANGUAGE,
+            useValue: of(TUI_ENGLISH_LANGUAGE)
+        },
+        tuiLanguageSwitcher(async (language: TuiLanguageName): Promise<unknown> => {
+            switch (language) {
+                case `es`:
+                    return import(`@taiga-ui/i18n/languages/spanish`);
+                default:
+                    return import(`@taiga-ui/i18n/languages/english`);
+            }
+        }),
+        provideAnimations(),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
+    .catch(err => console.log(err));
