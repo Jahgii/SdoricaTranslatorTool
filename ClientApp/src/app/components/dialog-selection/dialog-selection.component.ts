b2bcb9276c4152ea16e5ce20c47f4139e15c5d56
Jahg <jahg.huerta@gmail.com>
2023-11-22 18:43:31 -0800

    update name on server

--- ClientApp/src/app/components/dialog-selection/dialog-selection.component.ts
+++ ClientApp/src/app/components/dialog-selection/dialog-selection.component.ts
@@ -1,15 +1,16 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TuiElasticContainerModule, TuiInputInlineModule, TuiTreeModule } from '@taiga-ui/kit';
+import { TuiElasticContainerModule, TuiInputInlineComponent, TuiInputInlineModule, TuiProgressModule, TuiTreeModule } from '@taiga-ui/kit';
 import { DialogAssetsComponent } from '../dialog-assets/dialog-assets.component';
 import { IGroup, IMainGroup } from 'src/app/core/interfaces/i-dialog-group';
 import { ApiService } from 'src/app/core/services/api.service';
-import { map, mergeMap, toArray } from 'rxjs/operators';
+import { debounceTime, map, mergeMap, take, takeUntil, takeWhile, tap, toArray } from 'rxjs/operators';
 import { LanguageOriginService } from 'src/app/core/services/language-origin.service';
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription, firstValueFrom } from 'rxjs';
 import { TranslateModule } from '@ngx-translate/core';
 import { TuiButtonModule, TuiExpandModule, TuiScrollbarModule, TuiSvgModule } from '@taiga-ui/core';
 import { FormsModule } from '@angular/forms';
+import { error } from 'console';
 
 @Component({
   selector: 'app-dialog-selection',
@@ -25,6 +26,7 @@ import { FormsModule } from '@angular/forms';
     TuiElasticContainerModule,
     TuiExpandModule,
     TuiInputInlineModule,
+    TuiProgressModule,
 
     DialogAssetsComponent
   ],
@@ -39,6 +41,7 @@ export class DialogSelectionComponent implements OnInit, OnDestroy {
   private subsLanguage!: Subscription;
   public groupSelected$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   public hiddenScroll$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  private subsName: Subscription | undefined;
 
   constructor(
     private api: ApiService,
@@ -55,8 +58,8 @@ export class DialogSelectionComponent implements OnInit, OnDestroy {
           .pipe(
             map(array => array.map(
               g => {
-                g.editing = false;
                 g.expanded = false;
+                g.saving = new BehaviorSubject<boolean>(false);
                 return g;
               }
             ))
@@ -86,10 +89,10 @@ export class DialogSelectionComponent implements OnInit, OnDestroy {
 
   private groupQuery(lang: string, originalName: string) {
     return this.api
-      .getWithHeaders<IGroup[]>('groups', { language: lang, mainGroup: originalName })
+      .getWithHeaders<TreeNode[]>('groups', { language: lang, mainGroup: originalName })
       .pipe(map(array => array.map(
         g => {
-          g.editing = false;
+          g.saving = new BehaviorSubject<boolean>(false);
           return g;
         }
       ))
@@ -108,11 +111,69 @@ export class DialogSelectionComponent implements OnInit, OnDestroy {
 
   public onMouseLeave() {
     this.hiddenScroll$.next(true);
+    (document.activeElement as any)?.blur();
+  }
+
+  public onFocusName(focus: boolean, input: TuiInputInlineComponent, node: TreeNode) {
+    if (focus) {
+      this.subsName = input.control?.valueChanges
+        .pipe(
+          tap(_ => node.saving?.next(true)),
+          debounceTime(1000),
+        ).subscribe(async name => {
+          await this.onChangeName(node);
+          node.saving?.next(false);
+        });
+    }
+    else {
+      let cS = this.subsName;
+      if (node.saving?.value === true) {
+        node.saving?.pipe(take(2))
+          .subscribe(_ => {
+            if (_) return;
+            if (cS) cS.unsubscribe();
+          });
+      }
+      else {
+        if (cS) cS.unsubscribe();
+      }
+    }
+  }
+
+  private async onChangeName(node: TreeNode) {
+    let updateNode: IMainGroup = {
+      Id: node.Id,
+      Name: node.Name,
+      Language: node.Language,
+      OriginalName: node.OriginalName,
+      ImageLink: node.ImageLink,
+      Files: node.Files,
+      TranslatedFiles: node.TranslatedFiles,
+      Order: node.Order
+    };
+
+    let url = "";
+
+    if ((node as IGroup).MainGroup) {
+      url = 'groups';
+      (updateNode as IGroup).MainGroup = (node as IGroup).MainGroup;
+    }
+    else {
+      url = 'maingroups';
+    }
+
+    await firstValueFrom(this.api.put(url, updateNode))
+      .then(r => {
+
+      }, error => {
+
+      });
   }
 
 }
 
 interface TreeNode extends IMainGroup {
-  children?: readonly IGroup[];
+  children?: readonly TreeNode[];
   expanded?: boolean;
+  saving?: BehaviorSubject<boolean>;
 }
