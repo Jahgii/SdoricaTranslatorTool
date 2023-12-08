b8b4a2a9e942557e63957ae23b1a6d40da6af038
Jahg <jahg.huerta@gmail.com>
2023-12-07 16:15:25 -0800

    add login to viewers

--- ClientApp/src/app/core/viewers.ts
+++ ClientApp/src/app/core/viewers.ts
@@ -1,15 +1,16 @@
 import { Type } from "@angular/core"
 import { LocalizationComponent } from "../localization/localization.component";
 import { DialogMainComponent } from "../dialog-assets/dialog-main/dialog-main.component";
-import { LoadFileWizardComponent } from "../components/load-file-wizard/load-file-wizard.component";
 import { ExportTranslationGuestComponent } from "../export/export-main/export-main.component";
+import { LoginComponent } from "../components/login/login.component";
+import { ImportMainComponent } from "../import/import-main/import-main.component";
 
 export interface Viewers { [component: string]: Type<any> };
 
 export var viewers: Viewers = {
+    login: LoginComponent,
     localization: LocalizationComponent,
     dialogs: DialogMainComponent,
-    import: LoadFileWizardComponent,
-    // import: ImportMainComponent,
+    import: ImportMainComponent,
     export: ExportTranslationGuestComponent
 };
