c1f0621f2cef63b0461fad0195357b85677585b8
Jahg <jahg.huerta@gmail.com>
2024-05-17 14:40:05 -0700

    export gamedata worker oflline

--- ClientApp/src/app/core/workers/export-gam.worker.ts
+++ ClientApp/src/app/core/workers/export-gam.worker.ts
@@ -3,43 +3,116 @@
 import { encode } from '@msgpack/msgpack';
 import { IOnMessage, ProgressStatus } from '../interfaces/i-export-progress';
 import { IGamedata, IGamedataValue } from '../interfaces/i-gamedata';
+import { ExportPostMessage } from '../interfaces/i-export';
+import { AppModes } from '../enums/app-modes';
+import { ObjectStoreNames } from '../interfaces/i-indexed-db';
 
 addEventListener('message', async ({ data }) => {
+  let message: ExportPostMessage = data;
+
+  const completeMessage: IOnMessage = { maxPg: 100, pg: 0, blob: undefined, pgState: ProgressStatus.retrivingServerData };
+  postMessage(completeMessage);
+
+  if (message.appMode === AppModes.Offline) {
+    let request = indexedDB.open(message.dbName, message.dbVersion);
+    request.onerror = (event) => onErrorOpenDB(event);
+    request.onsuccess = (event) => onSuccessOpenDB(event, completeMessage, message);
+  }
+  else if (message.appMode === AppModes.Online) {
+    await onExportOnline(completeMessage, message);
+  }
+});
+
+function onErrorOpenDB(event: Event) {
+  postMessage("CANT OPEN DB ON WEB WORKER");
+}
+
+function onSuccessOpenDB(event: Event, completeMessage: IOnMessage, message: ExportPostMessage) {
+  let db = (event.target as any).result;
+  db.onerror = (event: Event) => onError(event);
+
+  onExportOffline(db, completeMessage, message);
+}
+
+function onError(event: Event) {
+  // console.log(`Database error: ${(event.target as IDBRequest).error?.message}`);
+}
+
+function onExportOffline(db: IDBDatabase, completeMessage: IOnMessage, message: ExportPostMessage) {
+  const transaction = db.transaction([ObjectStoreNames.GamedataValue]);
+
+  transaction.oncomplete = (event) => {
+    postMessage(completeMessage);
+  };
+
+  transaction.onerror = (event) => {
+    //Do nothing
+  };
+
   let values: IGamedataValue[] = [];
-  let decodeResult = data.decodeResult as IGamedata;
-  const message: IOnMessage = { maxPg: 100, pg: 0, blob: undefined, pgState: ProgressStatus.retrivingServerData };
-  postMessage(message);
+  const request = transaction
+    .objectStore(ObjectStoreNames.GamedataValue)
+    .openCursor();
+
+  request.onsuccess = (event) => {
+    let cursor = (event.target as IDBRequest).result as IDBCursorWithValue;
+    if (cursor) {
+      if ((cursor.value as IGamedataValue).Custom) {
+        values.push(cursor.value);
+      }
+      cursor.continue();
+    }
+    else {
+      completeMessage.pgState = ProgressStatus.retrivingServerDataSucess;
+      postMessage(completeMessage);
+      onCreateNewGam(completeMessage, message, values);
+    }
+  };
+
+  request.onerror = (event) => {
+    completeMessage.pgState = ProgressStatus.retrivingServerDataError;
+    postMessage(completeMessage);
+  };
+}
+
+async function onExportOnline(completeMessage: IOnMessage, message: ExportPostMessage) {
+  let values: IGamedataValue[] = [];
 
   let promise = fetch('api/gamedatavalues/export', {
     method: 'GET',
     headers: {
       "Content-Type": "application/json; charset=utf-8",
-      "Authorization": `Bearer ${data.token}`
+      "Authorization": `Bearer ${message.token}`
     }
   });
 
   await promise.then(
     async res => {
       values = await res.json();
-      message.pgState = ProgressStatus.retrivingServerDataSucess;
-      postMessage(message);
+      completeMessage.pgState = ProgressStatus.retrivingServerDataSucess;
+      postMessage(completeMessage);
     },
     error => {
-      message.pgState = ProgressStatus.retrivingServerDataError;
-      postMessage(message);
+      completeMessage.pgState = ProgressStatus.retrivingServerDataError;
+      postMessage(completeMessage);
     }
   );
 
+  await onCreateNewGam(completeMessage, message, values);
+}
+
+async function onCreateNewGam(completeMessage: IOnMessage, message: ExportPostMessage, values: IGamedataValue[]) {
   if (values.length <= 0) {
-    message.pgState = ProgressStatus.retrivingServerDataEmpty;
-    postMessage(message);
+    completeMessage.pgState = ProgressStatus.retrivingServerDataEmpty;
+    postMessage(completeMessage);
     return;
   }
 
-  message.pgState = ProgressStatus.replacingContent;
-  postMessage(message);
+  completeMessage.pgState = ProgressStatus.replacingContent;
+  postMessage(completeMessage);
 
   let category = 'BuffInfo';
+  let decodeResult = message.decodeResult as IGamedata;
 
   values.forEach(v => {
     let finalExportValue: any[] = [];
@@ -50,11 +123,11 @@ addEventListener('message', async ({ data }) => {
     decodeResult.C[category].D.push(finalExportValue);
   });
 
-  message.pgState = ProgressStatus.generatingNewFile;
-  message.maxPg = 100;
-  message.pg = 0;
+  completeMessage.pgState = ProgressStatus.generatingNewFile;
+  completeMessage.maxPg = 100;
+  completeMessage.pg = 0;
 
-  postMessage(message);
+  postMessage(completeMessage);
 
   let encodeResult = encode(decodeResult);
 
@@ -62,8 +135,8 @@ addEventListener('message', async ({ data }) => {
     type: ''
   });
 
-  message.pgState = ProgressStatus.finish;
-  message.blob = blob;
-  message.pg = 100;
-  postMessage(message);
-});
+  completeMessage.pgState = ProgressStatus.finish;
+  completeMessage.blob = blob;
+  completeMessage.pg = 100;
+  postMessage(completeMessage);
+}
