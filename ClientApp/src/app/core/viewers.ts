e4485573d4c02910707182898bc1ce3f2074ca07
Jahg <jahg.huerta@gmail.com>
2024-07-25 15:29:34 -0700

    remove login component

--- ClientApp/src/app/core/viewers.ts
+++ ClientApp/src/app/core/viewers.ts
@@ -2,7 +2,6 @@ import { Type } from "@angular/core"
 import { LocalizationComponent } from "../localization/localization.component";
 import { DialogMainComponent } from "../dialog-assets/dialog-main/dialog-main.component";
 import { ExportTranslationGuestComponent } from "../export/export-main/export-main.component";
-import { LoginComponent } from "../components/login/login.component";
 import { ImportMainComponent } from "../import/import-main/import-main.component";
 import { WizardInitialComponent } from "../components/wizard-initial/wizard-initial.component";
 import { AppLoadingComponent } from "../components/app-loading/app-loading.component";
@@ -10,7 +9,6 @@ import { AppLoadingComponent } from "../components/app-loading/app-loading.compo
 export interface Viewers { [component: string]: Type<any> };
 
 export var viewers: Viewers = {
-    login: LoginComponent,
     localization: LocalizationComponent,
     dialogs: DialogMainComponent,
     import: ImportMainComponent,
