98d56ba4294dbf6daf13c5849b9a71a9313c1173
Jahg <jahg.huerta@gmail.com>
2024-12-01 16:43:37 -0800

    dialog-aseet-single add common dictionary directive and hint to show other languages

--- ClientApp/src/app/dialog-assets/dialog-assets/dialog-asset-single/dialog-asset-single.component.ts
+++ ClientApp/src/app/dialog-assets/dialog-assets/dialog-asset-single/dialog-asset-single.component.ts
@@ -1,10 +1,16 @@
-import { Component, Inject, Input } from '@angular/core';
+import { Component, Inject, Input, OnInit, signal, WritableSignal } from '@angular/core';
 import { PortraitsService } from 'src/app/core/services/portraits.service';
 import { DialogAssetService } from '../dialog-asset.service';
 import { IDialog, IDialogAsset } from 'src/app/core/interfaces/i-dialog-asset';
 import { FormsModule } from '@angular/forms';
-import { AsyncPipe, NgIf, NgStyle } from '@angular/common';
+import { AsyncPipe, KeyValuePipe, NgFor, NgIf, NgStyle } from '@angular/common';
 import { ElementBreakpointService } from 'src/app/core/services/element-breakpoint.service';
+import { TuiHint, TuiIcon, TuiScrollbar } from '@taiga-ui/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { TranslateModule } from '@ngx-translate/core';
+import { TuiBlockStatus } from '@taiga-ui/layout';
+import { popinAnimation } from 'src/app/core/animations/popin';
+import { CommonDictionaryDirective } from 'src/app/core/directives/common-dictionary.directive';
 
 @Component({
   selector: 'app-dialog-asset-single',
@@ -14,20 +20,39 @@ import { ElementBreakpointService } from 'src/app/core/services/element-breakpoi
     AsyncPipe,
     NgStyle,
     NgIf,
+    NgFor,
+    KeyValuePipe,
+    CommonDictionaryDirective,
+
+    TranslateModule,
+
+    TuiHint,
+    TuiIcon,
+    TuiScrollbar,
+    TuiBlockStatus,
   ],
+  animations: [popinAnimation],
   templateUrl: './dialog-asset-single.component.html',
   styleUrl: './dialog-asset-single.component.scss',
 })
-export class DialogAssetSingleComponent {
+export class DialogAssetSingleComponent implements OnInit {
   @Input() data!: IDialogAsset[];
   @Input() dialogAsset!: IDialogAsset;
   @Input() item!: IDialog;
 
+  protected focus: WritableSignal<boolean> = signal(false);
+  protected otherText$!: Observable<any>;
+  protected showTooltipArrow$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
   constructor(
     private readonly portraitsService: PortraitsService,
-    @Inject(DialogAssetService) readonly dAS: DialogAssetService,
+    @Inject(DialogAssetService) public readonly dAS: DialogAssetService,
     @Inject(ElementBreakpointService) readonly breakpointService: ElementBreakpointService
   ) { }
+
+  ngOnInit(): void {
+    this.otherText$ = (this.dAS.onGetOtherOriginalText(this.data[this.dAS.activeItemIndex].Number, this.item.ID) as Subject<any>).asObservable();
+  }
 
   public onTextChange(dialogAsset: IDialogAsset) {
     this.dAS.onTextChange(dialogAsset);
@@ -39,5 +64,13 @@ export class DialogAssetSingleComponent {
 
   public onGetPortrait(name: string, ext: string = '.png') {
     return this.portraitsService.imageDir[name + ext];
+  }
+
+  public onTooltipCheck(scrollTooltip?: TuiScrollbar) {
+    let show = false;
+    if (scrollTooltip)
+      show = scrollTooltip['el']['offsetHeight'] < scrollTooltip['el']['scrollHeight'];
+
+    this.showTooltipArrow$.next(show);
   }
 }
