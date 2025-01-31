69aeb9d7083fa2d8105e98cf3dea62ffc7337ecb
Jahg <jahg.huerta@gmail.com>
2025-01-30 19:47:50 -0800

    electron remember windows position

--- ClientApp/main.js
+++ ClientApp/main.js
@@ -1,23 +1,44 @@
-const { app, BrowserWindow } = require('electron');
+const { app, screen, BrowserWindow } = require('electron');
+const Store = require('electron-store');
 const url = require('url');
 const path = require('path');
 
+const store = new Store();
 let mainWindow;
+let windowConfig = {
+    title: 'STT',
+    width: 1280,
+    height: 680,
+    icon: '/dist/assets/images/app-icon.svg',
+    minWidth: 1280,
+    minHeight: 680,
+    webPreferences: {
+        // preload: path.join(__dirname, 'preload.js'),
+        nodeIntegration: false,
+        devTools: false,
+    }
+};
+
+function getWindowsBounds() {
+    const winBounds = store.get('winBounds');
+    if (winBounds !== undefined) {
+        const screenArea = screen.getDisplayMatching(winBounds).workArea;
+        const widthCheck = (winBounds.x > screenArea.x + screenArea.width || winBounds.x < screenArea.x);
+        const heightCheck = (winBounds.y < screenArea.y || winBounds.y > screenArea.y + screenArea.height);
+        Object.assign(windowConfig, winBounds);
+
+        if (widthCheck || heightCheck) {
+            windowConfig.x = 0;
+            windowConfig.y = 0;
+        }
+    }
+}
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        title: 'STT',
-        width: 800,
-        height: 600,
-        icon: '/dist/assets/images/app-icon.svg',
-        minWidth: 800,
-        minHeight: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true,
-            devTools: true,
-        }
-    });
+    getWindowsBounds();
+    mainWindow = new BrowserWindow(windowConfig);
+
+    if (windowConfig.isMaximized) mainWindow.maximize();
 
     mainWindow.loadURL(
         url.format({
@@ -27,10 +48,15 @@ function createWindow() {
         })
     );
 
-    mainWindow.webContents.openDevTools();
+    mainWindow.on('close', () => {
+        Object.assign(windowConfig, {
+            isMaximized: mainWindow.isMaximized()
+        }, mainWindow.getNormalBounds())
+        store.set("winBounds", windowConfig);
+    });
 
-    mainWindow.on('closed', function () {
-        mainWindow = null
+    mainWindow.on('closed', () => {
+        mainWindow = null;
     });
 
     mainWindow.setMenu(null);
@@ -39,9 +65,9 @@ function createWindow() {
 app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', function () {
-    if (mainWindow === null) createWindow()
+    if (mainWindow === null) createWindow();
 });
