811222b6875346bd82e8375d31b5d81c939d7bfe
Jahg <jahg.huerta@gmail.com>
2025-11-09 14:35:35 -0800

    scan taiga icons

---
+++ Angular/scan-taiga-icons.js
@@ -0,0 +1,71 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const ICON_REGEXES = [
+    // matches iconStart/iconEnd with optional @tui.
+    /(?:\[?icon(?:Start|End)\]?=)["'](?:'|")?(?:@tui\.)?([a-zA-Z0-9-]+)(?:'|")?["']/g,
+    // matches icon
+    /(?:\[?icon\]?=)["'](?:@tui\.)?([a-zA-Z0-9-]+)["']/g,
+    // matches tui-icon background
+    /<tui-icon\b[^>]*?\[background\]=["'](?:'|")?(?:@tui\.)?([a-zA-Z0-9-]+)(?:'|")?["']/g,
+    // matches all @tui.
+    /@tui\.([a-zA-Z0-9-]+)/g
+];
+
+const htmlDir = path.join(__dirname, 'src');
+const iconSrcDir = path.join(__dirname, 'node_modules/@taiga-ui/icons/src');
+const iconDestDir = path.join(__dirname, 'src/assets/taiga-ui/icons/');
+
+const icons = new Set();
+
+function scanDir(dir) {
+    fs.readdirSync(dir).forEach(file => {
+        const fullPath = path.join(dir, file);
+        const stat = fs.statSync(fullPath);
+
+        if (stat.isDirectory()) {
+            scanDir(fullPath);
+        } else if (file.endsWith('.html')) {
+            const content = fs.readFileSync(fullPath, 'utf8');
+            ICON_REGEXES.forEach(regex => {
+                let match;
+                while ((match = regex.exec(content)) !== null) {
+                const icon = match[1] || match[0];
+                icons.add(icon);
+                }
+            });
+        }
+    });
+
+}
+
+function copyIcons() {
+    if (!fs.existsSync(iconDestDir)) {
+        fs.mkdirSync(iconDestDir, { recursive: true });
+    }
+    
+    icons.forEach(icon => {
+        const filename = `${icon}.svg`;
+        const src = path.join(iconSrcDir, filename);
+        const dest = path.join(iconDestDir, filename);
+
+        if (fs.existsSync(src)) {
+            fs.copyFileSync(src, dest);
+            console.log(`✓ Copied ${filename}`);
+        } else {
+            console.warn(`⚠️ Icon not found: ${filename}`);
+        }
+    });
+}
+
+function copyRequiredIcons() {
+    icons.add("check");
+    icons.add("eye");
+    icons.add("chevron-down");
+    icons.add("languages");
+    icons.add("minus");
+}
+
+scanDir(htmlDir);
+copyRequiredIcons();
+copyIcons();
