00e13259094d9094509a9525150cd712ac44aa7f
Jahg <jahg.huerta@gmail.com>
2025-12-01 20:25:35 -0800

    add fallback to backdrop-filter

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { ThemeService } from './core/services/theme.service';
 import { HeaderMenuComponent } from './components/header-menu/header-menu.component';
 import { RouterOutlet } from '@angular/router';
@@ -20,11 +20,36 @@ import { TuiSkeleton } from '@taiga-ui/kit';
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   protected readonly app = inject(AppStateService);
   protected readonly theme = inject(ThemeService);
   protected readonly translate = inject(LangService);
 
   public title = 'STT';
+
+  ngOnInit(): void {
+    this.setBrowserFlag();
+  }
+
+  private setBrowserFlag(): void {
+    const ua = navigator.userAgent;
+
+    let browser = '';
+    if (ua.includes('Firefox')) browser += 'Firefox ';
+    if (ua.includes('WebKitGTK')) browser += 'WebKitGTK ';
+    if (ua.includes('Chrome')) browser += 'Chrome ';
+    if (ua.includes('Safari')) browser += 'Safari ';
+    if (ua.includes('AppleWebKit')) browser += 'AppleWebKit ';
+    if (ua.includes('Edg')) browser += 'Edge ';
+    if (ua.includes('MSIE') || ua.includes('Trident')) browser += 'IE ';
+    if (ua.includes('Opera Mini')) browser += 'Opera Mini ';
+    if (ua.includes('UCBrowser')) browser += 'UC Browser ';
+    if (ua.includes('QQBrowser')) browser += 'QQBrowser ';
+    if (ua.includes('KaiOS')) browser += 'KaiOS ';
+
+    if (browser === '') browser = 'unknown';
+
+    document.documentElement.dataset.browser = browser;
+  }
 
 }
